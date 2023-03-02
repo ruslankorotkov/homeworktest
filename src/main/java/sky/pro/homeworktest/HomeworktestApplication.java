04980dccc5ab3d8fccc4d8a92a06c57488afe88d
ruslankorotkov <richard2008@rus>
2023-03-02 15:19:37 +0300

    Тестирование Урок 28.02.2023

---
+++ src/main/java/sky/pro/homeworktest/HomeworktestApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.homeworktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworktestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworktestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/homeworktest/HomeworktestApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.homeworktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworktestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
